9b40c46770dea3f2eb77d96b77a9b1e7a4edbba0
Dazhi Wang <dazhi.wang@desy.de>
2020-04-18 04:44:15 +0900

    Env setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+RELEASE=release-04-02-01
+
+source /cvmfs/belle.cern.ch/tools/b2setup
+b2setup $RELEASE
+
+# Deal with segmentation violation for some versions
+export LD_LIBRARY_PATH=/sw/belle/local/neurobayes-4.3.1/lib/:$LD_LIBRARY_PATH
+
+# Running on data for exp 31-65
+export USE_GRAND_REPROCESS_DATA=1
+
+# Set Belle DB
+export BELLE_POSTGRES_SERVER=can01
+
+# Access to Belle DB
+export PGUSER=g0db
+
